6d10c414bd4b238dff436fc89a2995f878407dc7
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2025-02-11 09:52:09 +0100

    init.

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1. Gör en tuple med siffror
+a = (1, 2, 3, 4, 5)
+
+# 2. Gör en tuple med dina favoritbilar
+b = ("bla", "bla", "bla")
+
+# 3. Använd join för att förena listan till en sträng med kommatecken mellan varje bil
+c = ",".join(b)
+print(c)
+
+# 4. Kolla på tre-i-rad-spelet och se hur join används där
+# Ok.
